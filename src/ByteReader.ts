dc3d8981d21284184237cbec58ce1b1b76328041
kion <kion@dashgl.com>
2025-03-16 10:12:03 -0500

    update to version 0.020

--- src/ByteReader.ts
+++ src/ByteReader.ts
@@ -2,25 +2,25 @@
     This file is part of ByteReader Library
     Copyright 2023 [Benjamin Collins](kion@dashgl.com)
 
-    Permission is hereby granted, free of charge, to any person obtaining 
+    Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the
-    "Software"), to deal in the Software without restriction, including 
-    without limitation the rights to use, copy, modify, merge, publish, 
-    distribute, sublicense, and/or sell copies of the Software, and to 
-    permit persons to whom the Software is furnished to do so, subject to 
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
     the following conditions:
 
-    The above copyright notice and this permission notice shall be included     
+    The above copyright notice and this permission notice shall be included
     in all copies or substantial portions of the Software.
 
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
-    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
-    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
     SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-    
+
 */
 
 export default class ByteReader {
@@ -127,5 +127,19 @@ export default class ByteReader {
 
   subArray(startOffset: number, endOffset: number): ArrayBuffer {
     return this.bytes.slice(startOffset, endOffset);
+  }
+
+  getSlice(length: number): ByteReader {
+    const startOffset = this.ofs;
+    const endOffset = startOffset + length;
+
+    // Create a new ByteReader with the sub-buffer
+    const subBuffer = this.bytes.slice(startOffset, endOffset);
+    const newReader = new ByteReader(subBuffer, this.littleEndian);
+
+    // Advance the current offset
+    this.ofs += length;
+
+    return newReader;
   }
 }
